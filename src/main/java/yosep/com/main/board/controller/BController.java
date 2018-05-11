bc08a204bf6ca884d5f2b079bb5f1a5a8df2f28f
전요셉 <jeon-yoseb@jeon-yoseb-ui-MacBook-Pro.local>
2018-05-11 22:42:06 +0900

    중간 커밋 list.jsp 관련 controller 및 command 로직 구현중...

---
+++ src/main/java/yosep/com/main/board/controller/BController.java
@@ -0,0 +1,68 @@
+package yosep.com.main.board.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BController {
+
+	@RequestMapping("/board/list")
+	public String list(Model model) {
+		System.out.println("list jsp 호출");
+
+		return "list";
+	}
+
+	@RequestMapping("/board/write_view")
+	public String write_view(Model model) {
+		System.out.println("write_view jsp 호출");
+
+		return "write_view";
+	}
+
+	@RequestMapping("/board/write")
+	public String write(HttpServletRequest request, Model model) {
+		System.out.println("write()");
+
+		return "list";
+	}
+
+	@RequestMapping("/board/content_view")
+	public String content_view(HttpServletRequest request, Model model) {
+		System.out.println("content_view()");
+
+		return "content_view";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/board/modify")
+	public String modify(HttpServletRequest request, Model model) {
+		System.out.println("modify()");
+
+		return "list";
+	}
+
+	@RequestMapping("/board/reply_view")
+	public String reply_view(HttpServletRequest request, Model model) {
+		System.out.println("reply_view(");
+		
+		return "reply_view";
+	}
+	
+	@RequestMapping("/board/reply")
+	public String reply(HttpServletRequest request, Model model) {
+		System.out.println("reply()");
+		
+		return "list";
+	}
+	
+	@RequestMapping("/board/delete")
+	public String delete(HttpServletRequest request, Model model) {
+		System.out.println("delete()");
+		
+		return "list";
+	}
+}
---
+++ src/main/java/yosep/com/main/board/dao/BDao.java
@@ -0,0 +1,66 @@
+package yosep.com.main.board.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import yosep.com.main.board.dto.BDto;
+
+public class BDao {
+	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplate;
+	private RowMapper<BDto> boardContentMapper = new RowMapper<BDto>() {
+
+		@Override
+		public BDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+			// TODO Auto-generated method stub
+			BDto dto = new BDto();
+			dto.setbId(rs.getInt("bId"));
+			dto.setbName(rs.getString("bName"));
+			dto.setbTitle(rs.getString("bTitle"));
+			dto.setbContent(rs.getString("bContent"));
+			dto.setbDate(rs.getTimestamp("bDate"));
+			dto.setbHit(rs.getInt("bHit"));
+			dto.setbGroup(rs.getInt("bGroup"));
+			dto.setbStep(rs.getInt("bStep"));
+			dto.setbIndent(rs.getInt("bIndent"));
+			return dto;
+		}
+	};
+
+	public BDao() {
+
+	}
+
+	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	public ArrayList<BDto> list() {
+		return (ArrayList<BDto>) jdbcTemplate.query(
+				"select bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent from mvc_board order by bGroup desc, bStep asc",
+				new RowMapper<BDto>() {
+
+					@Override
+					public BDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+						// TODO Auto-generated method stub
+						BDto dto = new BDto();
+						dto.setbId(rs.getInt("bId"));
+						dto.setbName(rs.getString("bName"));
+						dto.setbTitle(rs.getString("bTitle"));
+						dto.setbContent(rs.getString("bContent"));
+						dto.setbDate(rs.getTimestamp("bDate"));
+						dto.setbHit(rs.getInt("bHit"));
+						dto.setbGroup(rs.getInt("bGroup"));
+						dto.setbStep(rs.getInt("bStep"));
+						dto.setbIndent(rs.getInt("bIndent"));
+						return dto;
+					}
+				});
+	}
+}
